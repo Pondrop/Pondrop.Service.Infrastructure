55917b7c6ed8379f0260c3338cad0a9fa006f091
Bryan <Bryan@xam.com.au>
2022-11-21 15:23:31 +0000

    Merged PR 284: Added blob storage service
    Related work items: #1003

---
+++ Pondrop.Service.Infrastructure/Interfaces/BlobStorage/IBlobStorageService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pondrop.Service.Interfaces;
+
+public interface IBlobStorageService
+{
+    Task<string> UploadImageAsync(string fileName, string base64, string userId = "", string mimeType = null);
+}
